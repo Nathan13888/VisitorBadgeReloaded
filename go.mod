9301caaf6faee8b6bf53887b6211c1a5a0b5c2ea
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-05-11 08:09:54 -0400

    replace http router with gorilla mux
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Nathan13888/VisitorBadgeReloaded
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // defaults
@@ -23,19 +28,54 @@ var key = getEnv("KEY", "guess_what")
 
 // Visitor Badge URL Format: /badge?page_id=<key>
 func main() {
+	r := mux.NewRouter()
+	server := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
 	fmt.Printf("Starting Visitor Badge Reloaded Server...\n\n")
 
 	// TODO: DEFAULT_*{COLOUR,STYLE,TEXT,LOGO}
 
 	fmt.Printf("Key is set to `%s`\n", key)
 
-	http.HandleFunc("/", getWebsite)
-	http.HandleFunc("/index.html", getWebsite)
-	http.HandleFunc("/ping", getPing)
-	http.HandleFunc("/badge", getBadge)
+	r.HandleFunc("/", getWebsite).Methods("GET")
+	r.HandleFunc("/index.html", getWebsite).Methods("GET")
+	r.HandleFunc("/ping", getPing).Methods("GET")
+	r.HandleFunc("/badge", getBadge).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-	// log.Fatal(http.ListenAndServeTLS(":8081", "localhost.crt", "localhost.key", nil))
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	wait := 15 * time.Second
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	server.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down...")
+	os.Exit(0)
 }
 
 func getBadge(w http.ResponseWriter, r *http.Request) {
